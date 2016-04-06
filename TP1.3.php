ff6e2bb10e5f1dcca3d0e7ae4020687145409885
benjamindecastro <benjamindecastro@hotmail.fr>
2016-04-06 06:28:02 -0400

    exo 3

---
+++ TP1.3.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset= "UTF-8" />
+    <title></title>
+  </head>
+  <body>
+   <?php
+    $repertoire = array(
+    array("Moteur de recherche",
+    array("nom"=>"Google","image"=>,"url"=>"https://www.google.fr/"),
+    array("nom"=>"Yahoo","image"=>,"url"=>"https://fr.yahoo.com/"),
+    ),
+    array("Journaux",
+    array("nom"=>"LeMonde","image"=>,"url"=>"http://www.lemonde.fr/"),
+    array("nom"=>"Yahoo","image"=>,"url"=>"https://fr.yahoo.com/"),
+    )
+    ?>
+    
+  </body>
+</html>
