8e3f3d602a57de3d1315cce64ebf6dff7b480374
benjamindecastro <benjamindecastro@hotmail.fr>
2016-04-06 05:58:00 -0400

    first commit

---
+++ TP1.1.php
@@ -0,0 +1,13 @@
+<?php
+
+$clients = array ("Luc",7 => "Paul",2 =>"Martin","Arnaud");
+//0,7,2,8
+
+echo $clients[8];
+
+$produits = array (20 => "Chemise",3 => "Pantalon",10 => "Jupe","Veste","Blouson");
+//1
+
+echo $produits[22];
+
+?>
